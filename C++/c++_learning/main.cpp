ae91b78441e11e54a152a1da716a124687e62a1b
wshtx <2462899595htx@gmail.com>
2023-10-29 15:17:55 +0800

    udpate

--- C++/c++_learning/main.cpp
+++ C++/c++_learning/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "instrumentationTimer.h"
 
+
+
 int main()
 {
-    Instrumentor::Get().BeginSession("Profile");
-    RunBenchmarks();
-    Instrumentor::Get().EndSession();
-    std::cin.get();
+    ///chrome浏览器支持的可视化耗时分析
+//    Instrumentor::Get().BeginSession("Profile");
+//    RunBenchmarks();
+//    Instrumentor::Get().EndSession();
+
 }
